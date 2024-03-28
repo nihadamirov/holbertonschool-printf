187bb1f996ab60bc85b0cd0a4536dac2ad1842c7
Nihad <nihadamirov98@gmail.com>
2024-03-28 18:53:54 +0000

    update file

--- get_fun.c
+++ get_fun.c
@@ -1,7 +1,27 @@
 #include "main.h"
 
 /**
- *
- *
- */
+* get_print - get the correct function
+* @tipo: format specifier
+* Return: pointer to the correct function
+**/
+int (*get_print(char tipo))(va_list)
+{
+	int i;
+	print_t op[] = {
+		{'c', print_char},
+		{'s', print_string},
+		{'d', print_int},
+		{'i', print_int},
+		{'\0', NULL}
+	};
 
+	for (i = 0; op[i].type; i++)
+	{
+		if (tipo == op[i].type)
+		{
+			return (op[i].function);
+		}
+	}
+	return (NULL);
+}
